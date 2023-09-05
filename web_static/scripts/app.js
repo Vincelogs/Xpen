828eca30a5c1d1016d9e4eabd1f059447bcaa29a
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-09-05 16:05:03 +0300

    implement adding and displaying of expenses and updating the html file and some styles

--- web_static/scripts/app.js
+++ web_static/scripts/app.js
@@ -23,4 +23,3 @@ navLinks.forEach (function(link) {
     event.preventDefault();
   }); 
 });
-
--- web_static/scripts/expenses.js
+++ web_static/scripts/expenses.js
@@ -1,4 +1,4 @@
-export function showExpenses() {
+export function showExpenses () {
   const dashboard = document.getElementById('dashboard');
   const expenses = document.getElementById('expenses');
   const income = document.getElementById('income');
@@ -8,4 +8,72 @@ export function showExpenses() {
   expenses.style.display = 'block';
   income.style.display = 'none';
   profile.style.display = 'none';
+
+  const expenseList = document.getElementById('expense-transactions');
+  const addExpense = document.getElementById('addExpense');
+  const amountInput = document.getElementById('amount');
+  const nameInput = document.getElementById('name');
+  const dateInput = document.getElementById('date');
+  const categoryInput = document.getElementById('category');
+  let listExpenses = [];
+  let symbol = '$';
+
+// function to display expenses
+  function displayExpenses () {
+
+    // clearing existing content
+    expenseList.innerHTML = '';
+
+    if (listExpenses.length === 0) {
+      expenseList.innerHTML = '<h2>No Expense Found !</h2>';
+    } else {
+      listExpenses.forEach((e,index) => {
+        expenseList.innerHTML += ` 
+	<li class="expense-transactions">
+            <p>${e.category}</p>
+            <div>
+                <p>${e.name}</p>
+            </div>
+            <div>
+                <p>${e.date}</p>
+            </div>
+            <div class="content-right">
+                <p>${symbol}${e.amount}</p>
+                <button onclick="editExpense(${index})">Edit</button>
+                <button onclick="deleteExpense(${index})">Delete</button>
+            </div>
+        </li>
+        `;
+      });
+    }
+  }
+
+  // function to edit an expense
+  function editExpense (index) {
+  }
+
+  // function to delete an expense
+  function deleteExpense (index) {
+  }
+
+  // adding an expense
+  addExpense.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (amountInput.value <= 0 || nameInput.value === '' || dateInput.value === '') {
+      alert('Please enter a valid value.');
+      return;
+    }
+    let expense = {
+      amount: Number(amountInput.value),
+      name: nameInput.value,
+      date: dateInput.value,
+      category: categoryInput.value
+    };
+    console.log(expense);
+    listExpenses.push(expense);
+    displayExpenses();
+  });
+
+  displayExpenses();
 }
