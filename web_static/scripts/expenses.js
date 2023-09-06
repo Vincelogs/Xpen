8e3a3da2355d934154489cf6b6cf1731010ad3cb
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-09-06 18:58:03 +0300

    implement CRUD functions for expenses

--- web_static/scripts/expenses.js
+++ web_static/scripts/expenses.js
@@ -15,65 +15,141 @@ export function showExpenses () {
   const nameInput = document.getElementById('name');
   const dateInput = document.getElementById('date');
   const categoryInput = document.getElementById('category');
+  const cancelEdit = document.getElementById('cancelEdit');
   let listExpenses = [];
-  let symbol = '$';
+  const symbol = '$';
+  let editIndex = -1;
 
-// function to display expenses
-  function displayExpenses () {
-
-    // clearing existing content
+  // Function to display expenses
+  function displayExpenses (listExpenses) {
+    // Clear existing content
     expenseList.innerHTML = '';
 
     if (listExpenses.length === 0) {
-      expenseList.innerHTML = '<h2>No Expense Found !</h2>';
+      expenseList.innerHTML = '<h2>No expenses recorded yet.</h2>';
     } else {
-      listExpenses.forEach((e,index) => {
-        expenseList.innerHTML += ` 
-	<li class="expense-transactions">
-            <p>${e.category}</p>
-            <div>
-                <p>${e.name}</p>
-            </div>
-            <div>
-                <p>${e.date}</p>
-            </div>
-            <div class="content-right">
-                <p>${symbol}${e.amount}</p>
-                <button onclick="editExpense(${index})">Edit</button>
-                <button onclick="deleteExpense(${index})">Delete</button>
-            </div>
-        </li>
+      listExpenses.forEach((e, index) => {
+        expenseList.innerHTML += `
+          <li class="expense-transactions">
+              <p>${e.category}</p>
+              <div>
+                  <p>${e.name}</p>
+              </div>
+              <div>
+                  <p>${e.date}</p>
+              </div>
+              <div class="content-right">
+                  <p>${symbol}${e.amount}</p>
+                  <button data-id="${index}">Edit</button>
+                  <button data-id="${index}">Delete</button>
+              </div>
+          </li>
         `;
       });
     }
+    saveExpenses();
   }
 
-  // function to edit an expense
-  function editExpense (index) {
+  // Function to edit an expense
+  function editExpense (dataIndex) {
+    editIndex = dataIndex;
+    console.log(`Attempting to edit expense at index ${dataIndex}`);
+
+    // populate form fields with expense data
+    amountInput.value = listExpenses[dataIndex].amount;
+    nameInput.value = listExpenses[dataIndex].name;
+    dateInput.value = listExpenses[dataIndex].date;
+    categoryInput.value = listExpenses[dataIndex].category;
+
+    // display button to cancel an edit
+    cancelEdit.style.display = 'block';
   }
 
-  // function to delete an expense
-  function deleteExpense (index) {
+  // Function to delete an expense
+  function deleteExpense (dataIndex) {
+    console.log(`Attempting to delete expense at index ${dataIndex}`);
+    listExpenses = listExpenses.filter((e, idx) => idx !== dataIndex);
+    displayExpenses(listExpenses);
   }
 
-  // adding an expense
+  // Function to clear input fields
+  function clearInputs () {
+    amountInput.value = '';
+    nameInput.value = '';
+    dateInput.value = '';
+    categoryInput.value = '';
+  }
+
+  // function to store the Expense data
+  function saveExpenses () {
+    localStorage.setItem('expenselist', JSON.stringify(listExpenses));
+  }
+
+  // function to get the saved data
+  function getExpenses () {
+    const storedExpenses = localStorage.getItem('expenselist');
+    listExpenses = storedExpenses ? JSON.parse(storedExpenses) : [];
+  }
+
+  // Event delegation for "Edit" and "Delete" buttons
+  expenseList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+      const dataIndex = event.target.getAttribute('data-id');
+      if (dataIndex !== null) {
+        const expenseIndex = parseInt(dataIndex);
+        if (event.target.textContent === 'Edit') {
+          editExpense(expenseIndex);
+        } else if (event.target.textContent === 'Delete') {
+          deleteExpense(expenseIndex);
+        }
+      }
+    }
+  });
+
+  // Event to cancel edit and reset fields
+  cancelEdit.addEventListener('click', () => {
+    editIndex = -1;
+    amountInput.value = '';
+    nameInput.value = '';
+    dateInput.value = '';
+    categoryInput.value = '';
+    cancelEdit.style.display = 'none';
+  });
+
+  // Event listener for adding and editing an expense
   addExpense.addEventListener('click', (event) => {
     event.preventDefault();
 
     if (amountInput.value <= 0 || nameInput.value === '' || dateInput.value === '') {
-      alert('Please enter a valid value.');
+      alert('Please enter valid values.');
       return;
     }
-    let expense = {
+    const expense = {
       amount: Number(amountInput.value),
       name: nameInput.value,
       date: dateInput.value,
       category: categoryInput.value
     };
+
+    if (editIndex === -1) {
+      listExpenses.push(expense);
+    } else {
+      listExpenses[editIndex] = expense;
+    }
+
+    editIndex = -1;
     console.log(expense);
-    listExpenses.push(expense);
-    displayExpenses();
+
+    displayExpenses(listExpenses);
+
+    cancelEdit.style.display = 'none';
+
+    // Clear input fields after adding an expense
+    clearInputs();
   });
 
-  displayExpenses();
+  getExpenses();
+
+  // Initial display of expenses
+  displayExpenses(listExpenses);
 }
